bef66b4f7961637aaf13b53db4b67f79249bfa5b
koarks <keer9624@gmail.com>
2020-06-13 21:05:09 +0530

    Added extra approaches

--- Data Structures/Arrays and Strings/is_Unique.java
+++ Data Structures/Arrays and Strings/is_Unique.java
@@ -1,11 +1,13 @@
 // Implement an algorithm to determine if a string has all unique characters
 // Without using additional data structures
+
 import java.util.*;
 
 class is_Unique{
    public static void main(String args[]){
-      String str = "kKerthna";
-      System.out.println("using hash set - "+using_hashset(str));  
+      String str = "kerthna";
+      System.out.println("using hash set - "+using_hashset(str)); 
+      System.out.println("using bit vector - "+using_bitVector(str)); 
    }
    // using a data structure
    public static boolean using_hashset(String s){
@@ -18,8 +20,30 @@ class is_Unique{
             set.add(ch);
          else
             return false;         
-      }   
-      
+      }         
       return true;
    }
+   
+   // Using Bit Vector
+   // Time Complexity - O(n)
+   // Space Complexity - some extra space is used to store the intermediate bits
+   public static boolean using_bitVector(String s){
+      int checker = 0;
+      int val = 0;
+      
+      for(int i =0;i < s.length();i++){
+         val = s.charAt(i) - 'a';
+         if( (checker & ( 1 << val)) > 0)
+            return false;
+         
+         checker = checker | ( 1 << val);
+      }
+      
+      return true;   
+   }
+// Other approaches
+// 1. Brute Force - for every character in the string, run for the entire string to check if the character exists. This approach takes O(n^2)
+//    time complexity.
+// 2. Sort the input string, run through the string and check the neighbouring characters. sorting might take some space.
+
 }
